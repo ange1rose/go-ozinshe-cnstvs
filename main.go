cb85099352226034c7da4e4665ea03cd403b80ac
Constantin Vassilyev <ange1rose666@icloud.com>
2025-02-21 13:33:57 +0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	corsConfig := cors.Config{
+		AllowAllOrigins: true,
+		AllowHeaders:    []string{"*"},
+		AllowMethods:    []string{"*"},
+	}
+	r.Use(cors.New(corsConfig))
+	r.Run(":8081")
+}
